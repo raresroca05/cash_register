720f2350926889d51dbcbd9b627d9913edac10c1
Rares Roca <rares.roca05@gmail.com>
2024-07-21 16:51:48 +0300

    Add Rspec and Bootstrap + Configure them

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,8 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
+pin "application", preload: true
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+pin "bootstrap", to: "https://unpkg.com/bootstrap@5.1.3/dist/js/bootstrap.min.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
